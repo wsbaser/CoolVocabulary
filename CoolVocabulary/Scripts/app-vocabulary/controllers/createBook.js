694d74464a2111801373527f4c97f24d62194f0a
Alexey Volkov <wsbaser@gmail.com>
2015-10-08 23:30:45 +0400

    added "createBook" action

---
+++ CoolVocabulary/Scripts/app-vocabulary/controllers/createBook.js
@@ -0,0 +1,7 @@
+Vocabulary.CreateBookController = Ember.Controller.extend({
+	actions:{
+		createBook: function(){
+			console.log('create book action activated');
+		}
+	}
+});
--- CoolVocabulary/Scripts/app-vocabulary/routes/createBook.js
+++ CoolVocabulary/Scripts/app-vocabulary/routes/createBook.js
@@ -1,3 +1,8 @@
 Vocabulary.CreateBookRoute = Ember.Route.extend({
-	
+	model: function(){
+		var applicationCtrl = this.controllerFor('application');
+		return this.store.createRecord('book',{
+			language: applicationCtrl.get('langPair').sourceLang
+		});
+	}
 });
