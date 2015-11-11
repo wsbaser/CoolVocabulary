f3f7c0ab5efa339c57a9f37c3e996b7c6357c5fb
Alexey Volkov <wsbaser@gmail.com>
2015-11-11 16:26:12 +0300

    translation progress, book progress

--- CoolVocabulary/Scripts/app-vocabulary/helpers/learn-battery.js
+++ CoolVocabulary/Scripts/app-vocabulary/helpers/learn-battery.js
@@ -2,14 +2,19 @@ Vocabulary.LearnBatteryHelper = Ember.Helper.helper(function(params, namedParams
 	// . define min learning level of all translations
 	var level=params[0];
 
-	// . generate svg element
-	var svgBody = '';
-	for (var i = 1 ; i <=5; i++) {
-		var x = (i-1) * 3;
-		var color = (i<=level?
-				"#f55":  // . red
-				"#ccc"); // . grey
-		svgBody += '<rect x="'+ x +'" width="2" height="6" fill="' + color + '" style="opacity:0.6"></rect>';
+	if(level===MAX_LEARN_LEVEL){
+		return Ember.String.htmlSafe('<span class="glyphicon glyphicon-ok"/>');
 	}
-	return  Ember.String.htmlSafe('<svg class="learn-battery">' + svgBody + '</svg>');
+	else{
+		// . generate svg element
+		var svgBody = '';
+		for (var i = 1 ; i <=5; i++) {
+			var x = (i-1) * 3;
+			var color = (i<=level?
+					"#f55":  // . red
+					"#ccc"); // . grey
+			svgBody += '<rect x="'+ x +'" width="2" height="6" fill="' + color + '" style="opacity:0.6"></rect>';
+		}
+		return  Ember.String.htmlSafe('<svg class="learn-battery">' + svgBody + '</svg>');
+	}
 });
--- CoolVocabulary/Scripts/app-vocabulary/models/bookword.js
+++ CoolVocabulary/Scripts/app-vocabulary/models/bookword.js
@@ -4,25 +4,6 @@ Vocabulary.BookWord = DS.Model.extend({
     speachPart: DS.attr("number"),
     learnedAt: DS.attr("number"),
     translations: DS.hasMany("translation"),
-  //   init: function(){
-  //   	if(this.get('word.value')){
-  //   		this.updateLearnLevel();
-  //   	}
-  //   },
-  //   initLearnLevel: Ember.observer('translations.[]', function(){
-  //   	this.updateLearnLevel();
-  //   }),
-  //   updateLearnLevel: function(){
-  //   	console.log(this.get('word.value'));
-  //   	var level = MAX_LEARN_LEVEL;
-		// this.get('translations').forEach(function(item){
-		// 	var translationLevel = item.get('learnLevel') || 0;
-		// 	if(translationLevel<level){
-		// 		level = translationLevel;
-		// 	}
-		// });
-		// this.set('learnLevel', level);
-  //   },
     learnLevel: Ember.computed("translations.[]", function(){
     	var level = MAX_LEARN_LEVEL;
 		this.get('translations').forEach(function(item){
